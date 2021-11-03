587431a6c5deb83e83ee8b10b7ccb3329e0f446a
Andreas Menzel <mail@andreas-menzel.com>
2021-11-03 21:13:13 +0100

    Implemented 'player_thread_function' and extended Player.

--- centimenti.py
+++ centimenti.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.ui import WebDriverWait
+import threading
+import time
 
 
 class Player:
     driver = None
     game_code = ""
     player_name = ""
+    answers = []
 
-    def __init__(self, game_code, player_name):
+    def __init__(self, game_code, player_name, answers):
         self.driver = webdriver.Firefox()
+        self.driver.set_window_size(400, 600)
         self.game_code = game_code
         self.player_name = player_name
+        self.answers = answers
 
     def join_game(self):
         self.driver.get("https://menti.com")
@@ -31,5 +35,51 @@ class Player:
         elem_quiz_name_submit = self.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
         elem_quiz_name_submit.click()
 
-    def __del__(self):
-        self.driver.close()
+    def guess(self, answer):
+        print('Player "', self.player_name, '" guesses ', answer, '.',  sep='')
+        pass
+
+    def check_result(self):
+        # Maybe stop playing if answer was incorrect
+        pass
+
+    def cleanup(self):
+        self.driver.quit()
+
+
+def player_thread_function(game_code, player_name, answers):
+    print('Creating player "', player_name, '"...', sep='')
+    player = Player(game_code, player_name, answers)
+    print('Player "', player_name, '" successfully created. Now ready to join the game.', sep='')
+
+    event_join_game.wait()
+    print('Player "', player_name, '" is now joining the game...', sep='')
+    player.join_game()
+    print('Player "', player_name, '" successfully joined the game!', sep='')
+
+    for a in answers:
+        player.guess(a)
+        player.check_result()
+
+    time.sleep(5)
+    player.cleanup()
+
+
+
+event_join_game = threading.Event()
+
+
+if __name__ == "__main__":
+    threads = []
+    threads.append(threading.Thread(target=player_thread_function, args=("10491429", "Player 1", [0,1,2,3],)))
+    threads.append(threading.Thread(target=player_thread_function, args=("10491429", "Player 2", [1,2,3,0],)))
+    threads.append(threading.Thread(target=player_thread_function, args=("10491429", "Player 3", [2,3,0,1],)))
+    threads.append(threading.Thread(target=player_thread_function, args=("10491429", "Player 4", [3,0,1,2],)))
+    for t in threads:
+        t.start()
+
+    time.sleep(5)
+    event_join_game.set()
+
+    for t in threads:
+        t.join()
